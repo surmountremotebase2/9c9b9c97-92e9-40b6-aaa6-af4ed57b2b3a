9982f042d2cb9493af164d63b48bd8a7e80e36c6
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-10-26 19:34:11 +0500

    User Simon Flex saved changes to main.py at 10/26/2025, 14:34:10 UTC time.

--- 006dcb7b-b78b-4772-bd0f-f7ffdfbb9f76/main.py
+++ 006dcb7b-b78b-4772-bd0f-f7ffdfbb9f76/main.py
@@ -218,7 +218,7 @@ class TradingStrategy(Strategy):
             score_ma_150 * 0.08 + score_dir_150 * 0.05 + score_str_150 * 0.05
         )
         
-        raw_score = (weighted_score * 25) - (blend_pct_chg * 100) * 1.2
+        raw_score = (weighted_score * 25) - (blend_pct_chg * 100) * 1.3
         
         # 6. Smooth the score using a 10-day rolling average
         self.raw_roar_scores.append(raw_score)
@@ -236,10 +236,3 @@ class TradingStrategy(Strategy):
         log(f"SCORE {final_roar_score}")
         
         return TargetAllocation(self.last_alloc)
-
-
-
-
-
-
-
