f4c227747fe9bfa04cee8e46b0684a619b55dcf4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-10-26 20:30:29 +0500

    User Simon Flex saved changes to multiple files files at 10/26/2025, 15:30:28 UTC time.

--- 818d2972-9df6-402d-adc3-6cb83f726078/main.py
+++ 818d2972-9df6-402d-adc3-6cb83f726078/main.py
@@ -1,1 +1,32 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import TimMoore, AnalystLong
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [AnalystLong()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+        
+
+    def run(self, data):
+        tim_moore_holdings = data[("analyst_long",)]
+        allocations = {"AAPL":1}      
+        if tim_moore_holdings:
+            alloc_dict = tim_moore_holdings[-1]['allocations']
+            log(f"Trading: {tim_moore_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+            #allocations =  dict(list(alloc_dict.items())[-5:])
+        log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
