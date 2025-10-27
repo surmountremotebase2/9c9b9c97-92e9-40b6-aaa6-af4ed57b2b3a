30d286361fea6aa1ea1eced34c0d5cf50770781f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-10-28 00:10:46 +0500

    User Simon Flex saved changes to multiple files files at 10/27/2025, 19:10:45 UTC time.

--- 006dcb7b-b78b-4772-bd0f-f7ffdfbb9f76/main.py
+++ 006dcb7b-b78b-4772-bd0f-f7ffdfbb9f76/main.py
@@ -233,7 +233,7 @@ class TradingStrategy(Strategy):
         
         # Convert numpy floats to native Python floats to satisfy the assertion
         self.last_alloc = {"SPY": float(spy_weight), "BIL": float(bil_weight)}
-        #log(f"SCORE {final_roar_score}")
+        log(f"SCORE {final_roar_score}")
         
         return TargetAllocation(self.last_alloc)
 
