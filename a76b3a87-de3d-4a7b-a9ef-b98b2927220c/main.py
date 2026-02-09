c14bc3223cbbf612831831503d5afcf6f809a296
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-10 00:31:57 +0500

    User Simon Flex saved changes to main.py at 02/09/2026, 19:31:56 UTC time.

--- a76b3a87-de3d-4a7b-a9ef-b98b2927220c/main.py
+++ a76b3a87-de3d-4a7b-a9ef-b98b2927220c/main.py
@@ -22,22 +22,10 @@ class TradingStrategy(Strategy):
     def run(self, data):
         congress_buys_holdings = data[("congress_buys",)]
         allocations = {"BIL": 1}
-        if congress_buys_holdings:
+        if len(congress_buys_holdings) > 0:
             alloc_dict = congress_buys_holdings[-1]['allocations']
             #log(f"Trading: {congress_buys_holdings[-1]['allocations']}")
             allocations = alloc_dict
-            # If BBY is in the allocation, move its weight to BIL
-            if "BBY" in alloc_dict:
-                weight = alloc_dict.pop("BBY")      # remove BBY and get its allocation
-                alloc_dict["BIL"] = weight         # set BIL to that allocation
-            if "HY" in alloc_dict:
-                weight = alloc_dict.pop("HY")
-                alloc_dict["BIL"] = weight
-            if "MS-P" in alloc_dict:
-                weight = alloc_dict.pop("MS-P")
-                alloc_dict["BIL"] = weight
-            if "DTM" in alloc_dict:
-                weight = alloc_dict.pop("DTM")
-                alloc_dict["BIL"] = weight
-        log(f"allocations:{allocations}")
+            
+        #log(f"allocations:{allocations}")
         return TargetAllocation(allocations)
