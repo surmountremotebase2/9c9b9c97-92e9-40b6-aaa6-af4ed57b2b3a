639ecfe2539df7a1f071ca4aaa5494c2a9f1e959
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-16 01:02:51 +0500

    User Simon Flex saved changes to main.py at 02/15/2026, 20:02:50 UTC time.

--- da83d5d7-439b-40bc-a280-6142bab67438/main.py
+++ da83d5d7-439b-40bc-a280-6142bab67438/main.py
@@ -22,8 +22,8 @@ class TradingStrategy(Strategy):
     """
 
     def __init__(self):
-        self._assets = ["SPY", "QQQ", "TLT", "IEF", "BIL", "GLD"]
-        self.risk_assets = ["SPY", "QQQ", "TLT", "IEF", "GLD"]
+        self._assets = ["SPY", "QQQ", "TLT", "IEF", "BIL"]
+        self.risk_assets = ["SPY", "QQQ", "TLT", "IEF"]
         self.safe_asset = "BIL"
 
         self.rebalance_day = 1  # Tuesday
