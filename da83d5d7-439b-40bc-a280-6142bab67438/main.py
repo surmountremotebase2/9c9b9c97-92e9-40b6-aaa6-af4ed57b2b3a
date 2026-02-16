3491b45c0e4386bddcf68960a4a1ae4c6507ee06
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-16 13:41:13 +0500

    User Simon Flex saved changes to main.py at 02/16/2026, 08:41:12 UTC time.

--- da83d5d7-439b-40bc-a280-6142bab67438/main.py
+++ da83d5d7-439b-40bc-a280-6142bab67438/main.py
@@ -59,8 +59,8 @@ class TradingStrategy(Strategy):
                 low.rolling(period).min()) / 2
 
     def keltner_score(self, score_series):
-        mid = score_series.rolling(20).mean()
-        vol = score_series.rolling(20).std()
+        mid = score_series.rolling(50).mean()
+        vol = score_series.rolling(50).std()
         lower = mid - 2.5 * vol
         return mid.iloc[-1], lower.iloc[-1]
 
